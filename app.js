ad74b699c9bbb918a586594f78d2645cac084d6f
Cory Thompson <cory.thompson98@gmail.com>
2022-01-07 10:01:32 -0600

    updated template

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template.js');
 
-
 const promptUser = () => {
     return inquirer.prompt([
         {
@@ -41,24 +40,19 @@ const promptUser = () => {
             type: 'input',
             name: 'about',
             message: 'Provide some information about yourself:',
-            when: ({ confirmAbout }) => {
-                if (confirmAbout) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
+            when: ({ confirmAbout }) => confirmAbout
         }
     ]);
 };
 
-const promptProject = () => {
+const promptProject = portfolioData => {
     console.log(`
 =================
 Add a New Project
 =================    
 `);
 
+    // If there's no 'projects' array property, create one
     if (!portfolioData.projects) {
         portfolioData.projects = [];
     }
@@ -132,15 +126,14 @@ Add a New Project
     });
 };
 
-    promptUser()
-        .then(answers => console.log(answers))
-        .then(promptProject)
-        .then(projectAnswers => console.log(projectAnswers));
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        const pageHTML = generatePage(portfolioData);
 
-//const pageHTML = generatePage(name, github);
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err); 
 
-//fs.writeFile('./index.html', generatePage(name, github), err => {
-//    if (err) throw err; 
-
-//    console.log('Portfolio complete! Check out index.html to see the output');
-//});
+            console.log('Portfolio complete! Check out index.html to see the output');
+        });
+    });
